c95c33d2d1cd07ef5e5fef73f9356de4f97384c3
Joshua Scheel <jscheel42@gmail.com>
2018-08-18 15:05:29 -0700

    add build number tag

--- build.sh
+++ build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -e
 
-docker build -t jscheel42/build --no-cache --pull .
+docker build -t jscheel42/build:latest --no-cache --pull .
 if [ "$1" == "push" ]
 then
-    docker push jscheel42/build
+    docker push jscheel42/build:latest
+fi
+
+if [[ -v BUILD_NUMBER ]]
+then
+    docker tag jscheel42/build:latest jscheel42/build:$BUILD_NUMBER
+    docker push jscheel42/build:$BUILD_NUMBER
 fi
